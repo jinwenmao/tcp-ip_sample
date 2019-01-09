460a9928c4f0ac755d7ebb98f11af74b5022195f
jinwenmao <jinwenmao@139.com>
2019-01-09 16:02:16 +0800

    simple sample for c#

---
+++ TcpClientDemo/TcpClientDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpClientDemo
+{
+    class Program
+    {
+        static void Connect(String server, String message)
+        {
+            try
+            {
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                /* Translate the passed message into ASCII and store it as a Byte array. */
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                NetworkStream stream = client.GetStream();                
+                stream.Write(data, 0, data.Length);/* Send the message to the connected TcpServer.  */
+
+                Console.WriteLine("Sent: {0}", message);
+                data = new Byte[256];
+
+                /* String to store the response ASCII representation. */
+                String responseData = String.Empty;
+
+                Int32 bytes = stream.Read(data, 0, data.Length); /* Read the first batch of the TcpServer response bytes. */
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received from Server: {0}", responseData);
+
+                stream.Close();/* Close everything. */
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //Connect("192.168.0.1", "This is send from client");   //地址为服务器地址  127.0.0.1
+                Connect("127.0.0.1", "This is send from client");
+            }
+        }
+    }
+}
---
+++ TcpListenerDemo/TcpListenerDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpListenerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 13000;                /* Set the TcpListener (Server) on port 13000. */
+                server = new TcpListener(port);
+                server.Start(); /* Start listening for client requests. */
+                Byte[] bytes = new Byte[256];/* Buffer for reading data */
+                String data = null;
+                while (true)
+                {
+                    Console.Write("I'm Server,Waiting for a connection... ");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+                    data = null;
+                    NetworkStream stream = client.GetStream(); /* 获取一个stream对象来做reading和writing操作 */
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)/* Loop to receive all the data sent by the client. */
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);/* Translate data bytes to a ASCII string. */
+                        Console.WriteLine("Received From Client: {0}\n", data);
+                        data = data.ToUpper();/* Process the data sent by the client. */
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                        /* Send back a response. */
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Server Sent Back: {0} Server\n", data);
+                    }
+
+                    /* Shutdown and end connection */
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                /*  Stop listening for new clients. */
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
